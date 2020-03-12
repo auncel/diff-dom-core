6f74406a3cc53198cf4bf30f5744a3e77869eb67
yidafu <tiaozuwushi@qq.com>
2020-03-12 20:43:55 +0800

    refactor: delete src/RenderTree/renderNode

--- src/RenderTree/renderNode/css.ts
+++
@@ -1,7 +0,0 @@
-import { CSSProperties, PseudoClass } from '../const/css';
-
-export type TCSSProperty = typeof CSSProperties[number];
-
-export type TPseudoClass = typeof PseudoClass[number];
-
-export type TStyleProps = Partial<Record<TCSSProperty, string>>;
--- src/RenderTree/renderNode/element.ts
+++
@@ -1,7 +0,0 @@
-import { tags, AttributeList } from '../const/tag';
-
-export type TTag = typeof tags[number];
-
-export type TTagAttribute = typeof AttributeList[number];
-
-export type TAttributes = Partial<Record<TTagAttribute, string>>;
