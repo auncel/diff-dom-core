3c458832acbad4c1bd8f90dc65b7c79156432cee
yidafu <tiaozuwushi@qq.com>
2020-03-19 22:40:53 +0800

    fix: travis ci failed becase of case sensitivity

---
+++ src/evaluateSimilarity/DiffLog.interface.ts
@@ -0,0 +1,16 @@
+/* --------------------------------------------------------------------------*
+ * Description:                                                              *
+ *                                                                           *
+ * File Created: Friday, 13th March 2020 2:34 pm                             *
+ * Author: yidafu(dov-yih) (me@yidafu.dev)                                   *
+ *                                                                           *
+ * Last Modified: Friday, 13th March 2020 2:34 pm                            *
+ * Modified By: yidafu(dov-yih) (me@yidafu.dev>)                             *
+ *                                                                           *
+ * Copyright 2019 - 2020 Mozilla Public License 2.0                          *
+ *-------------------------------------------------------------------------- */
+
+export interface IDiffLog {
+  location: string;
+  difference: string[];
+}
