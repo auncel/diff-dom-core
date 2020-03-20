61830ff871eb33ddc8d8ee2455be9ba6989f19e0
yidafu <tiaozuwushi@qq.com>
2020-03-20 23:31:36 +0800

    test: fix failed test case

--- fixtures/readFixture.spec.ts
+++ fixtures/readFixture.spec.ts
@@ -8,7 +8,7 @@ describe('readFixture', () => {
     expect(simpleFixture.description).toBe('最简单的 div，只有宽高');
     expect(simpleFixture.fragment).toBe('<div></div>');
     expect(simpleFixture.stylesheet.replace(/\s/g, ''))
-      .toBe(`div{width:100px;height:100px;background-color:red;}`
+      .toBe(`div{width:100px;height:100px;background-color:rgb(255,0,0);}`
     );
   });
 });
@@ -16,10 +16,8 @@ describe('readFixture', () => {
 describe('readFixtures', () => {
   test('readFixtures work will', () => {
     const fixture = readFixtures(__dirname + '/elements/div/simple');
-    expect(fixture.question.fragment).toBe(fixture.answers[0].fragment);
-    expect(fixture.question.stylesheet).toBe(fixture.answers[0].stylesheet);
-    expect(fixture.question.fragment).toBe(fixture.answers[1].fragment);
-    expect(fixture.question.stylesheet).not.toBe(fixture.answers[1].stylesheet);
+    expect(fixture.question.fragment).toBe(fixture.answers[2].fragment);
+    expect(fixture.question.stylesheet).toBe(fixture.answers[2].stylesheet);
   });
 });
 
