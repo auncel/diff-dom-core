a35dbe5af97c2ef26ec421cd3c68d32e31abc80d
yidafu <tiaozuwushi@qq.com>
2020-05-26 10:50:13 +0800

    test: update test case

--- src/index.pptr.spec.ts
+++ src/index.pptr.spec.ts
@@ -24,20 +24,21 @@ import { diffDomCore, Puppeteer } from './index.pptr'
 
 import '../test/startup';
 import { getRenderTree } from '../test/getRenderTree';
+import { ElementRenderNode } from './RenderNode';
 
 jest.setTimeout(60_000);
 
 const fixtureMap = readAllFixtures();
 const similarityMap = new Map<string, IDiffResult>();
 
-const divSimple =  fixtureMap.get('elements\\div\\simple')!;
+const divSimple = fixtureMap.get('elements\\div\\simple')!;
 describe(divSimple.question.description, () => {
   const { question, answers, title } = divSimple;
   const [answer1, answer2, answer3] = answers;
 
   test(answer1.description, async () => {
-    const  questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
-    const evaluateResult: any = await diffDomCore(questionRenderTree, { html: answer1.fragment, style: answer1.stylesheet});
+    const questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
+    const evaluateResult: any = await diffDomCore(questionRenderTree, { html: answer1.fragment, style: answer1.stylesheet });
     evaluateResult.expect = answer1.similarity;
     expect(evaluateResult.score).toBe(answer1.similarity);
 
@@ -45,8 +46,8 @@ describe(divSimple.question.description, () => {
   });
 
   test(answer2.description, async () => {
-    const  questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
-    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer2.fragment, style: answer2.stylesheet});
+    const questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
+    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer2.fragment, style: answer2.stylesheet });
     (evaluateResult as any).expect = answer2.similarity;
     // FIXME: score is too heigh
     expect(evaluateResult.score).toBeGreaterThan(answer2.similarity);
@@ -55,8 +56,8 @@ describe(divSimple.question.description, () => {
   });
 
   test(answer3.description, async () => {
-    const  questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
-    const evaluateResult: any = await diffDomCore(questionRenderTree, { html: answer3.fragment, style: answer3.stylesheet});
+    const questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
+    const evaluateResult: any = await diffDomCore(questionRenderTree, { html: answer3.fragment, style: answer3.stylesheet });
     evaluateResult.expect = answer3.similarity;
     expect(evaluateResult.score).toBe(answer3.similarity);
 
@@ -65,22 +66,70 @@ describe(divSimple.question.description, () => {
 });
 
 
-const widthIs0 =  fixtureMap.get('elements\\div\\width-is-0')!;
-describe(divSimple.question.description, () => {
+const widthIs0 = fixtureMap.get('elements\\div\\width-is-0')!;
+describe(widthIs0.question.description, () => {
   const { question, answers, title } = widthIs0;
   const [answer1] = answers;
 
   test(answer1.description, async () => {
-    const  questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
-    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer1.fragment, style: answer1.stylesheet});
-    expect(evaluateResult.score).toBe(answer1.similarity);
-    expect(evaluateResult.logs.length).toBe(0);
+    const questionRenderTree = plainObject2RenderNode(await getRenderTree(divSimple.question));
+    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer1.fragment, style: answer1.stylesheet });
+    expect(evaluateResult.score).toBeGreaterThanOrEqual(answer1.similarity);
+    expect(evaluateResult.logs.length).toBe(1);
 
     (evaluateResult as any).expect = answer1.similarity;
     similarityMap.set(`${title}: ${question.name} ==> ${answer1.name}`, evaluateResult);
   });
 
 });
+
+
+const listGroup = fixtureMap.get('elements\\ul\\list-group')!;
+describe(listGroup.question.description, () => {
+  const { question, answers, title } = listGroup;
+  const [answer1, answer2, answer3, answer4] = answers;
+  let questionRenderTree: ElementRenderNode;
+  beforeAll(async () => {
+    questionRenderTree = plainObject2RenderNode(await getRenderTree(listGroup.question));
+  })
+
+  test(answer1.description, async () => {
+    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer1.fragment, style: answer1.stylesheet });
+    expect(evaluateResult.score).toBeGreaterThan(answer1.similarity);
+    expect(evaluateResult.logs.length).toBe(2);
+
+    (evaluateResult as any).expect = answer1.similarity;
+    similarityMap.set(`${title}: ${question.name} ==> ${answer1.name}`, evaluateResult);
+  });
+
+  test(answer2.description, async () => {
+    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer2.fragment, style: answer2.stylesheet });
+    expect(evaluateResult.score).toBeGreaterThan(answer2.similarity);
+    expect(evaluateResult.logs.length).toBe(3);
+
+    (evaluateResult as any).expect = answer2.similarity;
+    similarityMap.set(`${title}: ${question.name} ==> ${answer2.name}`, evaluateResult);
+  });
+
+  test(answer3.description, async () => {
+    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer3.fragment, style: answer3.stylesheet });
+    expect(evaluateResult.score).toBeGreaterThan(answer3.similarity);
+    expect(evaluateResult.logs.length).toBe(5);
+
+    (evaluateResult as any).expect = answer3.similarity;
+    similarityMap.set(`${title}: ${question.name} ==> ${answer3.name}`, evaluateResult);
+  });
+
+  test(answer4.description, async () => {
+    const evaluateResult = await diffDomCore(questionRenderTree, { html: answer4.fragment, style: answer4.stylesheet });
+    expect(evaluateResult.score).toBeGreaterThan(answer4.similarity);
+    expect(evaluateResult.logs.length).toBe(1);
+
+    (evaluateResult as any).expect = answer4.similarity;
+    similarityMap.set(`${title}: ${question.name} ==> ${answer4.name}`, evaluateResult);
+  });
+});
+
 
 afterAll(async () => {
   // const dateStr = new Date().toLocaleString().replace(/[,:\s\/]/g, '-');
