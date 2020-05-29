52ae4b98c073edf0ff52526b575127a89e41b135
yidafu <tiaozuwushi@qq.com>
2020-05-29 15:35:51 +0800

    fix: used-var error

--- src/index.dom.ts
+++ src/index.dom.ts
@@ -13,14 +13,14 @@
 import './types';
 // import '@auncel/common/polyfill/toJSON';
 import { IDiffResult } from './evaluateSimilarity/generateDiffResult.interface';
-import { createHTMLTpl, setConfig, getConfig } from './utils';
+import { setConfig, getConfig } from './utils';
 import { IHTMLSnippet } from './HTMLSnippet.interface';
 import { fixedScoringPointGenerateDiffResult } from './evaluateSimilarity';
 import { xTreeDiffPlustGenerateDiffTree } from './DiffTree/xTreeDiffPlusGenerateDiffTree';
 import ElementRenderNode, { IElementRenderNode } from './RenderNode/ElementRenderNode';
 import { plainObject2RenderNode } from './DiffTree/x-tree-diff-plus/plainObject2RenderNode';
 import './pptr/startup';
-import { pptrGenerateRenderTree } from './RenderTree/pptrGenerateRenderTree';
+// import { pptrGenerateRenderTree } from './RenderTree/pptrGenerateRenderTree';
 import { IDomDiffCoreOption } from './config';
 import { generateRenderTree } from './RenderTree';
 
@@ -28,7 +28,7 @@ import { generateRenderTree } from './RenderTree';
 export async function diffDomCore(
   question: IElementRenderNode | IHTMLSnippet, answer: IHTMLSnippet, options?: IDomDiffCoreOption,
 ): Promise<IDiffResult> {
-  const answerHtml = createHTMLTpl(answer.html, answer.style);
+  // const answerHtml = createHTMLTpl(answer.html, answer.style);
 
   setConfig('generation', options?.generation ?? {});
   setConfig('diff', options?.diff ?? {});
@@ -45,14 +45,14 @@ export async function diffDomCore(
     answerRenerTree = generateRenderTree() as ElementRenderNode;
     questionRenderTree = plainObject2RenderNode(question as IElementRenderNode);
   } else {
-    const questionHtml = createHTMLTpl(
-      (question as IHTMLSnippet).html,
-      (question as IHTMLSnippet).style,
-    );
-    [questionRenderTree, answerRenerTree] = await Promise.all([
-      pptrGenerateRenderTree(answerHtml),
-      pptrGenerateRenderTree(questionHtml),
-    ]);
+    // const questionHtml = createHTMLTpl(
+    //   (question as IHTMLSnippet).html,
+    //   (question as IHTMLSnippet).style,
+    // );
+    // [questionRenderTree, answerRenerTree] = await Promise.all([
+    //   pptrGenerateRenderTree(answerHtml),
+    //   pptrGenerateRenderTree(questionHtml),
+    // ]);
   }
   const diffTree = xTreeDiffPlustGenerateDiffTree(questionRenderTree!, answerRenerTree!);
   evaluateResult = fixedScoringPointGenerateDiffResult(diffTree);
